bb875e31319e163e48db2e32a37ebbf5292e6c99
Nathan Bookham <nathan@bookham.me.uk>
2013-06-28 10:22:01 +0100

    Update install.py

--- install.py
+++ install.py
@@ -18,37 +18,38 @@ print ""
 print "Checking internet connection..."
 connection_active = check_internet_connection()
 if connection_active == False:
-    print "Connection failed!"
+    print "Connection failed! Check your connection and proxy setings."
     exit()
 elif connection_active == True:
     print "Connection is active!"
 
 print ""
-print "Getting latest package lists..."
+print "Getting latest package lists... (This may take a while)"
 call (["sudo apt-get update"], shell=True)
 print "Package lists retrived!"
 
 print ""
-print "Updating packages..."
-call (["sudo apt-get -y upgrade"], shell=True)
+print "Updating packages... (This may take a while)"
+call (["sudo apt-get -y upgrade"], shell=False)
 print "Packages updated!"
 
 print ""
 print "Installing dependencies..."
-call (["sudo apt-get -y install wget libusb-1.0-0 python-setuptools"], shell=True)
-call (["sudo easy_install pyusb"], shell=True)
+call (["sudo apt-get -y install wget libusb-1.0-0 python-setuptools"], shell=False)
+call (["sudo easy_install pyusb"], shell=False)
 print "Dependencies installed!"
 
 print ""
 print "Downloading USBArm module..."
-call (["wget -O /tmp/usbarm.py.tmp https://raw.github.com/inversesandwich/USBArm/master/usbarm.py"], shell=True)
+call (["wget -O /tmp/usbarm.py.tmp https://raw.github.com/inversesandwich/USBArm/master/usbarm.py"], shell=False)
 print "Module downloaded!"
 
 print ""
 print "Preparing module..."
-call (["mv /tmp/usbarm.py.tmp ~/usbarm.py"], shell=True)
+call (["mv /tmp/usbarm.py.tmp ~/usbarm.py"], shell=False)
 print "Preparation complete!"
 
 print ""
-print "The USBArm module is stored in your home directory. See the readme on GitHub for information on usage."
-sleep(1)
+print "The USBArm module is stored in your home directory."
+print "See the readme on GitHub at http://github.com/inversesandwich/USBArm for information on usage."
+raw_input("Press enter to close...")
